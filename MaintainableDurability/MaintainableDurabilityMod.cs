43a1cee166179c4a887fc9ca3620e5a3d4ded2d3
1x0DE5CA <42442639+1x0DE5CA@users.noreply.github.com>
2026-08-26 02:17:23 +0300

    add: a Mod derived class that makes the mod's setting visible in the GUI

---
+++ MaintainableDurability/MaintainableDurabilityMod.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace MaintainableDurability
+{
+    public class MaintainableDurabilityMod : Mod
+    {
+        public MaintainableDurabilityMod(ModContentPack content) : base(content) { }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            var listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            MaintainableDurabilitySettings.MdDamageFactor = listingStandard.SliderLabeled(
+                $"Maintenance Durability damage factor: {MaintainableDurabilitySettings.MdDamageFactor:F2}",
+                MaintainableDurabilitySettings.MdDamageFactor,
+                0f,
+                1f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory() => "Maintainable Durability";
+    }
+}

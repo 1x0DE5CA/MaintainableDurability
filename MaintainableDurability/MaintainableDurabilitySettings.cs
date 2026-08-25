cd08d366c1ca09ef43fcd8b76b54efe2d7892c62
1x0DE5CA <42442639+1x0DE5CA@users.noreply.github.com>
2026-08-26 02:15:43 +0300

    add: a ModSettings derived class that holds settings for the mod

---
+++ MaintainableDurability/MaintainableDurabilitySettings.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace MaintainableDurability
+{
+    public class MaintainableDurabilitySettings : ModSettings
+    {
+        public static float MdDamageFactor = 0.5f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref MdDamageFactor, "MdDamageFactor", 0.5f);
+            base.ExposeData();
+        }
+    }
+}
